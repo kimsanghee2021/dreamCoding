21d11d12db1cef7c11bf0b93a3d9b8774dfd3009
kimsanghee2021 <tkdgmltjddnr@naver.com>
2021-07-15 21:46:52 +0900

    javascript navMenu click scrtion scroll
    네비게이션 메뉴 클릭시 각 해당 섹션 영역으로 부드럽게 이동 하는 것을 구현하였다.

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,12 +1,11 @@
-'use strict';
-//스크롤 했을 시 네비게이션 바가 불투명해지게 만들고 다시 올라가면 투명하게 만들어라
-
+'use stirct';
+/* 스크롤 했을 시 스크롤의 거리값과 네비게이션 높이값 이상이 되었을시 색상이 변경되게끔 적용시켜라 */
 const navBar = document.querySelector('#navbar');
 const navBarHeight = navBar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', function(){
     //console.log(window.scrollY);
-    //console.log(navBarHeight);
+    //console.log(`navBarHeight : ${navBarHeight}`);
     if(window.scrollY > navBarHeight){
         navBar.classList.add('navbar--dark');
     }else{
@@ -14,14 +13,16 @@ document.addEventListener('scroll', function(){
     }
 });
 
-//handle scrolling when tapping on the navbar menu
-// 각 버튼을 클릭시 해당 영역으로 스크롤링 이동하기
-const navBarMenu = document.querySelector('.navbar__menu');
-navBarMenu.addEventListener('click', function(){
+/* 네비게이션 탭을 클릭시 각 해당 영역으로 슬라이드 이동하기 */
+const navBarMenu = document.querySelector('.navbar__menu'); 
+navBarMenu.addEventListener('click', function(event){
 
     const target = event.target;
     const link = target.dataset.link;
+    //console.log(link);
     if(link == null){
         return;
-    }console.log(event.target.dataset.link);
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
 });
