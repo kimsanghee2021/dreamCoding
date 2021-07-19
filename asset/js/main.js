b63f094128c8ba4ff19ae9a257477fb3752d57ba
kimsanghee2021 <tkdgmltjddnr@naver.com>
2021-07-19 22:32:44 +0900

    JS My Work 버튼 클릭시 css 변경해주기

--- asset/js/main.js
+++ asset/js/main.js
@@ -74,7 +74,12 @@ workBtnContainer.addEventListener('click', (e)=>{
     if(filter == null){
         return;
     }
-    console.log(filter);
+    /* My Work 버튼 클릭시 버튼 css 변경해주기 */
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    e.target.classList.add('selected');
+
     projects.forEach(function(project){
         if(filter ==='*' || filter == project.dataset.type){
             project.classList.remove('invisible');
