09137c3b6b6b19aee316be470f7528e2783308ed
kimsanghee2021 <tkdgmltjddnr@naver.com>
2021-07-24 17:27:00 +0900

    use strict

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,4 @@
-'use stirct';
+'use strict';
 /* 스크롤 했을 시 스크롤의 거리값과 네비게이션 높이값 이상이 되었을시 색상이 변경되게끔 적용시켜라 */
 const navBar = document.querySelector('#navbar');
 const navBarHeight = navBar.getBoundingClientRect().height;
